fd00d7c850b72a6e15595cc19746efb3f961dc99
kunalsaraf <saraffkunal@gmail.com>
2020-03-30 21:31:20 +0530

    Modified Api Calling File

--- Client/callingMyApi.py
+++ Client/callingMyApi.py
@@ -5,7 +5,8 @@ import json
 # Initializing the needful
 img_location = 'index.jpeg'
 file_extention = img_location.split('.')[1]
-url = 'http://127.0.0.1:5000/third'
+localhost_url = 'http://127.0.0.1:5000/third'
+server_url = 'http://sipher.codes/third'
 
 # Opening the image and encoding it so that it can be transferred
 with open(img_location,'rb') as img_file:
@@ -15,7 +16,9 @@ with open(img_location,'rb') as img_file:
 my_json_data = {'ext':file_extention, 'media':string_of_image}
 
 # Making a post request to server and sending the json file created above
-response_sent_by_server = requests.post(url, json = my_json_data)
+print("Making Post Requests to Server")
+response_sent_by_server = requests.post(server_url, json = my_json_data)
+print(response_sent_by_server)
 
 # Converting response json file received from server to python dictionary
 my_dict = json.loads(response_sent_by_server.text)
@@ -25,3 +28,4 @@ file_name = 'My_image.' + my_dict['ext']
 img = open(file_name,'wb')
 img.write(base64.b64decode(my_dict['media']))
 img.close()
+print("File Successfully Created")
